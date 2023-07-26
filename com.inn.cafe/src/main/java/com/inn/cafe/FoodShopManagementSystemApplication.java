648557ab18aa4731c4b57f770eb0730d4f3a6007
anupamavm <63632098+anupamavm@users.noreply.github.com>
2023-07-26 16:16:48 +0530

    Spring Project Initiated

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/FoodShopManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inn.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodShopManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodShopManagementSystemApplication.class, args);
+	}
+
+}
